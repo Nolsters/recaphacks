049ed9feeb1a27eefc981ed3ea3a44f10c68ab50
Nolsters <nolanhepworth@gmail.com>
2022-01-30 03:46:39 -0500

    Fixed Deployment Python Part 3

--- routes/index.js
+++ routes/index.js
@@ -17,6 +17,15 @@ router.post('/youtubeUpload', function(req, res, next){
   console.log(req.body.youtube)
   var youtubeLink = req.body.youtube;
   const spawn = require("child_process").spawn;
+  const pythonProcess = spawn('python',[path.join(__dirname, '../scripts/test.py')]);
+  console.log('Process Spawned')
+  pythonProcess.stdout.on('data', function (data) {
+    console.log('Pipe data from python script ...');
+    dataToSend = data.toString();
+    res.send(dataToSend)
+   });
+   /*
+  const spawn = require("child_process").spawn;
   const pythonProcess = spawn('python',[path.join(__dirname, '../scripts/speech.py'), youtubeLink]);
   console.log('Process Spawned')
   pythonProcess.stdout.on('data', function (data) {
@@ -25,6 +34,7 @@ router.post('/youtubeUpload', function(req, res, next){
     console.log(dataToSend)
     res.send(dataToSend)
    });
+   */
    // in close event we are sure that stream from child process is closed
 })
 
