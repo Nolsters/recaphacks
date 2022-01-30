4783114c51120b75c9c6829ae552b0b905ec9ce2
Nolsters <nolanhepworth@gmail.com>
2022-01-30 03:05:53 -0500

    Fixed HTML

--- routes/index.js
+++ routes/index.js
@@ -6,24 +6,12 @@ const path = require('path')
 const multer  = require('multer') //use multer to upload blob data
 const upload = multer(); // set multer to be the upload variable (just like express, see above ( include it, then use it/set it up))
 
-const {Storage} = require('@google-cloud/storage');
-
-const gc = new Storage({
-  keyFilename: path.join(__dirname, '../speach-339718-f1b9b69dce93.json'),
-  projectId: 'speach-339718'
-})
-
-
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('home');
 });
 
-
-router.get('/home', function(req, res, next){
-  res.render('home');
-});
 
 router.post('/youtubeUpload', function(req, res, next){
   console.log(req.body.youtube)
